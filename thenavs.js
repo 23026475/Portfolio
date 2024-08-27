238013259cd59147d25f085636c7bbb9b5e3003a
23026475 <ndivhuswiswi@gmail.com>
2024-08-27 04:58:43 +0200

    fixed footer and api issues on home page

---
+++ thenavs.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll('.links');
+const currentPage = window.location.pathname;
+
+navLinks.forEach(link => {
+    if (link.href.includes(currentPage)) {
+        link.classList.add('active');
+    }
+});
